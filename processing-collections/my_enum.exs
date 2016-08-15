345d8d3e8aef5783f4df3a0729f94b2b55294417
Oren Kanner <okanner@gmail.com>
2016-08-15 09:06:02 -0400

    WIP: MyEnum

---
+++ processing-collections/my_enum.exs
@@ -0,0 +1,19 @@
+defmodule MyEnum do
+  def all?([], _), do: true
+  def all?([head | tail], func), do: func.(head) && all?(tail, func)
+
+  def each([], _), do: []
+  def each([head | tail], func), do: [func.(head) | each(tail, func)]
+
+  def filter([], _), do: []
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  def split([], _), do: []
+  def split([], 0)
+end
