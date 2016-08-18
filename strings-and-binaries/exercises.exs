b1006e80ff1c70b2446955d67798ca8f8f3bdce2
Oren Kanner <okanner@gmail.com>
2016-08-18 06:24:17 -0400

    rename eqn_parser to calculate

--- strings-and-binaries/exercises.exs
+++ strings-and-binaries/exercises.exs
@@ -5,27 +5,30 @@ defmodule StringFuncs do
 
   def anagram?(word1, word2), do: Enum.sort(word1) == Enum.sort(word2)
 
-  def eqn_parser(str) do
-    _eqn_parser(str, 0, 0, '')
+  def calculate(str) do
+    _calculate(str, 0, 0, '')
   end
 
-  defp _eqn_parser('', num1, num2, op) when num1 >= 0 and num2 >= 0 do
+  defp _calculate('', num1, num2, op) when num1 >= 0 and num2 >= 0 do
     case op do
       ?+ -> num1 + num2
       ?- -> num1 - num2
       ?* -> num1 * num2
-      ?/ -> num1 / num2
+      ?/ -> div(num1, num2)
       _ -> raise ArgumentError, message: "Invalid string"
     end
   end
-  defp _eqn_parser([h|t], num1, num2, '') when h in ?0..?9 do
-    _eqn_parser(t, num1*10 + (h - ?0), num2, '')
+  defp _calculate([h|t], num1, num2, op) when h == ?\s do
+    _calculate(t, num1, num2, op)
   end
-  defp _eqn_parser([h|t], num1, num2, '') do
-    _eqn_parser(t, num1, num2, h)
+  defp _calculate([h|t], num1, num2, '') when h in ?0..?9 do
+    _calculate(t, num1*10 + (h - ?0), num2, '')
   end
-  defp _eqn_parser([h|t], num1, num2, op) do
-    _eqn_parser(t, num1, num2*10 + (h - ?0), op)
+  defp _calculate([h|t], num1, num2, '') do
+    _calculate(t, num1, num2, h)
   end
-  defp _eqn_parser(_, _, _, _), do: raise ArgumentError, message: "Invalid string"
+  defp _calculate([h|t], num1, num2, op) do
+    _calculate(t, num1, num2*10 + (h - ?0), op)
+  end
+  defp _calculate(_, _, _, _), do: raise ArgumentError, message: "Invalid string"
 end
