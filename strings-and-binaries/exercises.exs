104f87afa08ca3da623979434e94eb6e4a4e05b0
Oren Kanner <okanner@gmail.com>
2016-08-18 07:37:33 -0400

    implement center

--- strings-and-binaries/exercises.exs
+++ strings-and-binaries/exercises.exs
@@ -31,4 +31,26 @@ defmodule StringFuncs do
     _calculate(t, num1, num2*10 + (h - ?0), op)
   end
   defp _calculate(_, _, _, _), do: raise ArgumentError, message: "Invalid string"
+
+  def center(strs) do
+    total_length = _find_length(strs)
+    strs
+    |> Enum.map(&(_pad_string(&1, total_length)))
+    |> Enum.join("\n")
+    |> IO.puts
+  end
+
+  defp _find_length(strs) do
+    strs
+    |> Enum.sort(&(String.length(&1) > String.length(&2)))
+    |> Enum.at(0)
+    |> String.length
+  end
+
+  defp _pad_string(string, total_length)  do
+    my_length = String.length(string)
+    string
+    |> String.rjust(my_length + div(total_length - my_length, 2))
+    |> String.ljust(total_length)
+  end
 end
