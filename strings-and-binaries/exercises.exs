3933b1233a17352573d0dbbcd1e58a56fa939274
Oren Kanner <okanner@gmail.com>
2016-08-17 20:47:59 -0400

    add anagram checker

--- strings-and-binaries/exercises.exs
+++ strings-and-binaries/exercises.exs
@@ -2,4 +2,6 @@ defmodule StringFuncs do
   def printable?([]), do: true
   def printable?([h|t]), do: printable?(h) && printable?(t)
   def printable?(c), do: c >= 32 && c <= 126
+
+  def anagram?(word1, word2), do: Enum.sort(word1) == Enum.sort(word2)
 end
